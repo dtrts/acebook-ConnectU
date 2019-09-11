db6afa8495b2a00c1d16404cca87e58e935ef58a
dtrts <djt210@gmail.com>
2019-09-11 16:28:45 +0100

    Added feature spec for delete button

---
+++ spec/features/post_delete_button_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.feature 'Delete post', type: :feature do
+  scenario 'Can delete own post' do
+    sign_in
+    # user = current_user
+    visit '/posts/new'
+    fill_in :"post[message]", with: 'This is a message'
+    click_button 'Submit'
+    # visit "/users/#{user.id}/posts"
+    click_button 'Delete Post'
+    expect(page).to have_content('Post successfully deleted.')
+  end
+  scenario 'Cannot delete other user post' do
+    sign_in
+    visit '/posts/new'
+    fill_in :"post[message]", with: 'This is a message'
+    click_button 'Submit'
+    sign_out
+    sign_in
+    visit '/posts'
+    expect(page).not_to have_button('Delete Post')
+  end
+end
