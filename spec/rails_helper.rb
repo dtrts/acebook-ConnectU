809687a84ddabb4540868e0091a21f90584c8f53
dtrts <djt210@gmail.com>
2019-09-11 11:22:52 +0100

    Added feature tests for deleting posts

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,8 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'clearance/rspec'
+require "support/features/clearance_helpers" # put this here to allow feature tests to be run file by file
+
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
