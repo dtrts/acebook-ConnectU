867c846928f0772830b84718a423524a2e39cf01
fentonhaslam <fentonhaslam@gmail.com>
2019-09-18 15:36:51 +0100

    added overwritten to user_from_params from clearance

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,19 +15,37 @@ class UsersController < Clearance::UsersController
 
   # protected
   def url_after_create
-    '/profile'
+    "/profile"
   end
 
   def show
     unless User.exists?(params[:id])
-      flash[:error] = 'That user does not exist'
+      flash[:error] = "That user does not exist"
       redirect_to posts_url
       # redirect_back && return
     end
 
     @posts = Post.where(
       "(user_id = '#{params[:id]}' and to_user_id is null) or to_user_id = '#{params[:id]}'"
-    ).order('created_at DESC')
+    ).order("created_at DESC")
     @wall_post = Post.new
+  end
+
+  private
+
+  def user_from_params
+    email = user_params.delete(:email)
+    password = user_params.delete(:password)
+    username = user_params.delete(:username)
+
+    Clearance.configuration.user_model.new(user_params).tap do |user|
+      user.email = email
+      user.password = password
+      user.username = username
+    end
+  end
+
+  def user_params
+    params[Clearance.configuration.user_parameter] || Hash.new
   end
 end
