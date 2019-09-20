5a285d977a2bc910ba5e506631325dd712eba24b
dtrts <djt210@gmail.com>
2019-09-20 14:19:44 +0100

    upped chat history

--- app/controllers/room_controller.rb
+++ app/controllers/room_controller.rb
@@ -1,5 +1,5 @@
 class RoomController < ApplicationController
   def show
-    @messages = Message.order(created_at: :asc).last(25)
+    @messages = Message.order(created_at: :asc).last(250)
   end
 end
